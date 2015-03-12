34aa3afd57b224a5f1b4ef6c8b9729eb08d93900
Chris Angelico <rosuav@gmail.com>
2015-03-12 15:47:34 +1100

    Add stubby argparse

--- snippets.py
+++ snippets.py
@@ -1,3 +1,5 @@
+import sys
+import argparse
 import logging
 
 # Set the log output file, and the log level
@@ -21,3 +23,12 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()

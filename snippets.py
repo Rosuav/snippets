2ce62f24ec149bba4cc91be52d620ee14c7ea3b5
Chris Angelico <rosuav@gmail.com>
2015-04-28 13:20:11 +1000

    Rework to use docstringargs to demonstrate the simplicity

--- snippets.py
+++ snippets.py
@@ -1,5 +1,6 @@
+"""Store and retrieve snippets of text"""
 import sys
-import argparse
+from docstringargs import cmdline
 import logging
 
 # Set the log output file, and the log level
@@ -7,55 +8,25 @@ logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 data = {}
 
+@cmdline
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
+    """Store a snippet with an associated name.
 
-    Returns the name and the snippet
+    name: The name of the snippet
+    snippet: The snippet text
     """
     logging.info("Storing({!r}, {!r})".format(name, snippet))
     data[name] = snippet
     return name, snippet
 
+@cmdline
 def get(name):
     """Retrieve the snippet with a given name.
 
-    If there is no such snippet, returns None
-
-    Returns the snippet.
+    name: The name of the snippet
     """
     logging.error("Retrieving({!r})".format(name))
     return data.get(name)
 
-def main():
-    """Main function"""
-    logging.info("Constructing parser")
-    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
-
-    # Subparser for the put command
-    logging.debug("Constructing put subparser")
-    put_parser = subparsers.add_parser("put", help="Store a snippet")
-    put_parser.add_argument("name", help="The name of the snippet")
-    put_parser.add_argument("snippet", help="The snippet text")
-
-    # Subparser for the get command
-    logging.debug("Constructing get subparser")
-    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
-    get_parser.add_argument("name", help="The name of the snippet")
-
-    arguments = parser.parse_args(sys.argv)
-
-    arguments = vars(arguments)
-    command = arguments.pop("command")
-
-    if command == "put":
-        name, snippet = put(**arguments)
-        print("Stored {!r} as {!r}".format(snippet, name))
-    elif command == "get":
-        snippet = get(**arguments)
-        print("Retrieved snippet: {!r}".format(snippet))
-
 if __name__ == "__main__":
-    main()
+    print("Result: {!r}".format(cmdline.main()))
